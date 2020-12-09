3bba9ca648259d28390bd3f3798281e9e822c8a7
icyair100 <34012403+icyair100@users.noreply.github.com>
2020-12-09 17:41:08 -0500

    Ps5b
    Ps5b

--- PS4/routes/ps4.js
+++ PS4/routes/ps4.js
@@ -3,11 +3,41 @@ const router = express.Router();
 const fetch = require('node-fetch');
 const CONFIG = require('../config/appConfig');
 
+const redis = require('redis');
+const client = redis.createClient();
+const {promisify} = require('util');
+
+const asyncSet = promisify(client.set).bind(client);
+const asyncGet = promisify(client.get).bind(client);
+const asyncExists = promisify(client.exists).bind(client);
+const asyncExpire = promisify(client.expire).bind(client);
+
+client.flushdb((err, response) => {
+    if (err) {throw new Error('flushing error')}
+});
 const getData = async joke => {
+    let match = await asyncExists(joke);
+    if (match){
+        let jsData = await asyncGet(joke);
+        let response = {
+            jokes: jsData,
+            cached: true
+        }
+        // console.log(response);
+        return response;
+    } else {
     console.log("Fetch data from external API")
     const rawData = await fetch(CONFIG.fetchAPI.url + "/" + joke.toString());
     const jsData = await rawData.json();
-    return jsData;
+    await asyncSet(joke, jsData);
+    let response = {
+        jokes: jsData,
+        cached: false
+    }
+    await asyncExpire(joke, 15);
+    // console.log(response);
+    return response;
+    }
 }
 
 // mounting process matches localhost:3000/ps4
@@ -18,9 +48,9 @@ router.route('/')
 
         //incoming external data render to back-end
         getData(req.body.jokes)
-            .then(jsData => res.render('ps4',
+            .then(response => res.render('ps4',
                 {
-                    'jokes' : jsData,
+                    'jokes' : response.jokes,
                 }    ))
             .catch(error => console.log("Failed to fetch JSON"))
     })
